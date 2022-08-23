d50581d6a82277f490accd31aaf0b61e86472afb
nohwooyeol <dnfdufdl@naver.com>
2022-08-23 11:27:25 +0900

    XXS Filter 기능 추가

--- src/main/java/com/kelog/kelog/cors/WebConfig.java
+++ src/main/java/com/kelog/kelog/cors/WebConfig.java
@@ -1,14 +1,24 @@
 package com.kelog.kelog.cors;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.navercorp.lucy.security.xss.servletfilter.XssEscapeServletFilter;
+import com.kelog.kelog.filter.HTMLCharacterEscapes;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
+@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
+
+    private final ObjectMapper objectMapper;
 
 
     @Override
@@ -22,6 +32,29 @@ public class WebConfig implements WebMvcConfigurer {
                 .exposedHeaders("*");
 
     }
+
+
+
+
+    //Lucy Xss filter 적용
+    @Bean
+    public FilterRegistrationBean xssFilterBean(){
+        FilterRegistrationBean registrationBean = new FilterRegistrationBean();
+        registrationBean.setFilter(new XssEscapeServletFilter());
+        registrationBean.setOrder(Ordered.LOWEST_PRECEDENCE);
+        registrationBean.addUrlPatterns("*.do", "*.jsp");
+        return registrationBean;
+    }
+
+    //requestBody xss 필터 적용(json/api)
+    @Bean
+    public MappingJackson2HttpMessageConverter jsonEscapeConverter() {
+        ObjectMapper copy = objectMapper.copy();
+        copy.getFactory().setCharacterEscapes(new HTMLCharacterEscapes());
+        return new MappingJackson2HttpMessageConverter(copy);
+    }
+
+
 }
 //    @Override
 //    public void addResourceHandlers(ResourceHandlerRegistry registry) {
@@ -34,3 +67,5 @@ public class WebConfig implements WebMvcConfigurer {
 //    }
 //}
 //
+
+
